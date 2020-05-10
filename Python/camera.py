73111c9d34299e599f7a3fc9655aa536fa18da3d
Ricky Rosa <ricardorcidad@gmail.com>
2020-05-09 22:15:47 -0400

    Integrated workflow v1. Missing QR Code.

--- Python/camera.py
+++ Python/camera.py
@@ -25,7 +25,6 @@ ser.write(b'D')
 
 
 cam = cv2.VideoCapture(1)
-
 cv2.namedWindow("test")
 img_counter = 0
 # print(my_path)
@@ -40,6 +39,7 @@ while True:
 
     k = cv2.waitKey(1)
     # print(k)
+
     if k%256 == 27:
         # ESC pressed
         print("Escape hit, closing...")
@@ -47,38 +47,40 @@ while True:
     elif k%256 == 114:
         ser.write(b'B')
         ser.write(b'D')
-    elif k%256 == 32:
+    # elif k%256 == 32:
         # SPACE pressed
+    ser.write(b'B')
+    ser.write(b'D')
+
+    img_name = "opencv_frame_{}.png".format(img_counter)
+    cropped = frame[100:360, 120:520]
+    # save image -----------------------------------------------------
+    # my_path = os.path.abspath(os.path.dirname(__file__))
+    # path = os.path.join(my_path, "/negative'")
+    # cv2.imwrite(os.path.join(path , img_name), cropped)
+    # print("{} written!".format(img_name))
+    # ----------------------------------------------------------------
+    # smart prediction ----------------------------------------------
+    # prediction = classifier.predict(np.array([cropped.flatten()]))
+    # print(prediction)
+    # if prediction[0] == 1:
+    #     print("CONGRATULATIONS! You're COVID-19 NEGATIVE")
+    # else:
+    #     print("We're sorry. You tested POSITIVE :(")
+    # ----------------------------------------------------------------
+    edges = cv2.Canny(cropped,100,200)
+    # print(edges.sum())
+    if edges.sum() < 1000:
         ser.write(b'B')
         ser.write(b'D')
-
-        img_name = "opencv_frame_{}.png".format(img_counter)
-        cropped = frame[100:360, 120:520]
-        # save image -----------------------------------------------------
-        # my_path = os.path.abspath(os.path.dirname(__file__))
-        # path = os.path.join(my_path, "/negative'")
-        # cv2.imwrite(os.path.join(path , img_name), cropped)
-        # print("{} written!".format(img_name))
-        # ----------------------------------------------------------------
-        # smart prediction ----------------------------------------------
-        # prediction = classifier.predict(np.array([cropped.flatten()]))
-        # print(prediction)
-        # if prediction[0] == 1:
-        #     print("CONGRATULATIONS! You're COVID-19 NEGATIVE")
-        # else:
-        #     print("We're sorry. You tested POSITIVE :(")
-        # ----------------------------------------------------------------
-        edges = cv2.Canny(cropped,100,200)
-        print(edges.sum())
-        if edges.sum() < 190000:
-            print("CONGRATULATIONS! You're COVID-19 NEGATIVE")
-            ser.write(b'C')
-        else:
-            print("We're sorry. You tested POSITIVE :(")
-            ser.write(b'A')
+    elif edges.sum() < 190000:
+        print("CONGRATULATIONS! You're COVID-19 NEGATIVE")
+        ser.write(b'C')
+    else:
+        print("We're sorry. You tested POSITIVE :(")
+        ser.write(b'A')
 
         # img_counter += 1
 
 cam.release()
-
 cv2.destroyAllWindows()
--- Python/runner.py
+++ Python/runner.py
@@ -3,6 +3,12 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+import numpy as np
+from sklearn import svm
+import cv2
+import os
+import serial
+
 # Initialize firebase app
 cred = credentials.Certificate("hack-covid-19-e4018-firebase-adminsdk-pt6vv-e257520af7.json")
 app = firebase_admin.initialize_app(cred)
@@ -25,16 +31,57 @@ def write_test_result_to_db(result):
         return 1
     except:
         return 0
-
+def clear_leds():
+    ser.write(b'B')
+    ser.write(b'D')
 
 hard_coded_stand_id = "HrPMcrft5L0k4nH6MZNC"
-qrcode_data = ""  # unlock code obtained from scanning qrcode
+qrcode_data = "7dd85cd4-86c1-4801-9882-2b795baef2d8" # unlock code obtained from scanning qrcode
 try:
     test_stand_ref = db.collection("test-stands").document(hard_coded_stand_id)
     test_stand_data = test_stand_ref.get().to_dict()
+    print(test_stand_data["unlockCode"])
     if test_stand_data["unlockCode"] == qrcode_data:
-        # TODO: implement functionality to run test using Alfie, and run write_test_result_to_db
-        pass
+        # functionality to run test using Alfie, and run write_test_result_to_db
+        # -----------------------------------------------------------------------
+        ser = serial.Serial('/dev/cu.usbmodem14601', 9600)
+        clear_leds()
+        cam = cv2.VideoCapture(1)
+        cv2.namedWindow("test")
+        while True:
+            ret, frame = cam.read()
+            if not ret:
+                print("failed to grab frame")
+                break
+            cv2.imshow("test", frame)
+
+            k = cv2.waitKey(1)
+
+            if k%256 == 27:
+                # ESC pressed
+                print("Escape hit, closing...")
+                break
+            elif k%256 == 114:
+                # R pressed. This resets LEDs
+                clear_leds()
+            elif k%256 == 32:
+                # SPACE pressed
+                clear_leds()
+                cropped = frame[100:360, 120:520]
+                edges = cv2.Canny(cropped,100,200)
+
+                if edges.sum() < 190000:
+                    print("CONGRATULATIONS! You're COVID-19 NEGATIVE")
+                    ser.write(b'C')
+                    write_test_result_to_db("NEGATIVE")
+                else:
+                    print("We're sorry. You tested POSITIVE :(")
+                    ser.write(b'A')
+                    write_test_result_to_db("POSITIVE")
+
+        cam.release()
+        cv2.destroyAllWindows()
+        # -----------------------------------------------------------------------
     else:
         print("QR Code does not correspond to this testing stand. Please try again or verify that you are using the"
               "correct stand.")
